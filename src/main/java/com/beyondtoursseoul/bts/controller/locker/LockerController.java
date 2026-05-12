9693e5fcb380f355dfc597b4938c99593377a02a
suhyeon <tngutnqls6909@gmail.com>
2026-05-13 02:03:46 +0900

    fix: 미사용 api 주석처리

--- src/main/java/com/beyondtoursseoul/bts/controller/locker/LockerController.java
+++ src/main/java/com/beyondtoursseoul/bts/controller/locker/LockerController.java
@@ -67,31 +67,31 @@ public class LockerController {
         return ResponseEntity.ok(lockerService.getLockerDetail(id, lang));
     }
 
-    /// 물품보관함 데이터 호출 api
-    @Operation(
-            summary = "물품보관함 외부 API 테스트 호출(3개)",
-            description = "서울시 열린데이터 광장 API로부터 실시간 물품보관함 데이터를 가져와 반환합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "데이터 호출 성공"),
-            @ApiResponse(responseCode = "500", description = "외부 API 통신 오류")})
-    @GetMapping("/test")
-    public ResponseEntity<LockerApiResponseDto> testLockerApi() {
-        return ResponseEntity.ok(lockerService.fetchLockerData());
-    }
-
-    /// DB에 물품보관함 데이터 insert, update 하는 메서드 - 번역 포함 (2주에 한 번 스케쥴링 설정)
-    @Operation(
-            summary = "물품보관함 데이터 DB 동기화 (임의호출X - 토큰 제한)",
-            description = "외부 API 데이터를 호출하여 DB(Supabase)에 최신 정보를 Insert 또는 Update합니다. 번역 데이터도 함께 갱신됩니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "동기화 성공"),
-            @ApiResponse(responseCode = "500", description = "데이터베이스 작업 실패")})
-    @PostMapping("/sync")
-    public ResponseEntity<String> syncLockerData() {
-        log.info("컨트롤러 진입");
-        lockerService.syncLockerDataToDb();
-        return ResponseEntity.ok("데이터 동기화 완료! DB(Supabase)를 확인해보세요.");
-    }
+//    /// 물품보관함 데이터 호출 api
+//    @Operation(
+//            summary = "물품보관함 외부 API 테스트 호출(3개)",
+//            description = "서울시 열린데이터 광장 API로부터 실시간 물품보관함 데이터를 가져와 반환합니다.")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "200", description = "데이터 호출 성공"),
+//            @ApiResponse(responseCode = "500", description = "외부 API 통신 오류")})
+//    @GetMapping("/test")
+//    public ResponseEntity<LockerApiResponseDto> testLockerApi() {
+//        return ResponseEntity.ok(lockerService.fetchLockerData());
+//    }
+//
+//    /// DB에 물품보관함 데이터 insert, update 하는 메서드 - 번역 포함 (2주에 한 번 스케쥴링 설정)
+//    @Operation(
+//            summary = "물품보관함 데이터 DB 동기화 (임의호출X - 토큰 제한)",
+//            description = "외부 API 데이터를 호출하여 DB(Supabase)에 최신 정보를 Insert 또는 Update합니다. 번역 데이터도 함께 갱신됩니다.")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "200", description = "동기화 성공"),
+//            @ApiResponse(responseCode = "500", description = "데이터베이스 작업 실패")})
+//    @PostMapping("/sync")
+//    public ResponseEntity<String> syncLockerData() {
+//        log.info("컨트롤러 진입");
+//        lockerService.syncLockerDataToDb();
+//        return ResponseEntity.ok("데이터 동기화 완료! DB(Supabase)를 확인해보세요.");
+//    }
 
     /// 번역 메서드 (주석)
 //    @PostMapping("translate-test")
